cec2d8b67f1fc4c732a75ff41cbbc7ef15fb6074
anushishukla <anushishukla2001@gmail.com>
2024-03-05 19:14:50 +0530

    Changes done

--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import {MenuList} from '../data/data'
 import Layout from '../Component/Layout'
 import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material'
 
 const Menu = () => {
+
+
+  const [meal, setMeal] = useState(null);
+
+  useEffect(() => {
+  
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+      .then(response => response.json())
+      .then(data => {
+     // console.log(data);
+        if(data.meals && data.meals.length > 0) {
+          setMeal(data.meals[0]);
+        }
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []); 
+
+
+  if (!meal) {
+    return <div>Loading...</div>;
+  }
+
+
+  
   return (
     <Layout>
    <Box sx={{justifyContent:"center", display:"flex", flexWrap:"wrap"}}>
